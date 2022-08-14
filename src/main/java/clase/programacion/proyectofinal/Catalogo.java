50a3dbe6efe562b606247392a7e799400118a7d9
Emiliano <Emiliano@Emiliano-Dell-G15>
2022-08-14 01:11:22 -0600

    Esta es la parte de mantenimiento de peliculas. Ya casi termino, solo me falta resolver un problema con la tabla que no acepta valores por alguna razon.

--- src/main/java/clase/programacion/proyectofinal/Catalogo.java
+++ src/main/java/clase/programacion/proyectofinal/Catalogo.java
@@ -6,8 +6,10 @@ package clase.programacion.proyectofinal;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.*;
 import javax.swing.event.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -17,191 +19,158 @@ public class Catalogo extends JFrame implements ActionListener, ChangeListener{
         
     private JLabel l1,l2,l3,l4,l5;
     private JLabel peli1, peli2, peli3, peli4;
-    public String pel1, pel2, pel3, pel4, pel5, pel6, pel7, pel8;
+    public String sala1,sala2,sala3,sala4,tP,aD,dM,M;
+    
+    private JButton b1;
     
     private ImageIcon peliDune, peliThor, peliJoker, peliTG;
     
-    private JComboBox sel1, sel2, sel3, sel4;
+    private JComboBox sel1, sel2;
     
-    private JTextField tf;
+    private JTextField tf, tf2, tf3;
     private JTextArea peliculas;
     private JScrollPane sc;
     
-    Catalogo cat;
     
-    String peli = "";
+    public static final String[] column ={"Pelicula", "Sala", "Edad"};
     
+    DefaultTableModel tbl = new DefaultTableModel(column,0);
+    private JTable mantPeli = new JTable(tbl);
+    ArrayList<Peliculas> listaPeli = new ArrayList<Peliculas>();
+    
+        
     public static void main(String[] args) {
         Catalogo cat = new Catalogo();
-        cat.setBounds(0,0,1200,720);
+        cat.setBounds(0,0,1920,1080);
         cat.setVisible(true);        
     }
     
     public Catalogo(){
         setLayout(null);
         
+        //String Salas 
+        sala1 = new String("Sala 1");
+        sala2 = new String("Sala 2");
+        sala3 = new String("Sala 3");
+        sala4 = new String("Sala 4");
+        //String Edad
+        tP = new String("Todo Publico");
+        aD = new String("12 +");
+        dM = new String("16+");
+        M = new String("18+");  
+        
         l1 = new JLabel("Mantenimiento de Peliculas",SwingConstants.CENTER);
-        l1.setBounds(-5,50,360,30);
-        l1.setFont(new Font("Arial",Font.BOLD,20));      
-             
-        //Catalogo de Peliculas y sus Directorios                
-        peliDune = new ImageIcon("C:\\Users\\Emiliano\\Pictures\\Cartelera\\Dune.jpg");
-        peliThor = new ImageIcon("C:\\Users\\Emiliano\\Pictures\\Cartelera\\Thor- amor y trueno.jpg");
-        peliJoker = new ImageIcon("C:\\Users\\Emiliano\\Pictures\\Cartelera\\Joker.jpg");
-        peliTG = new ImageIcon("C:\\Users\\Emiliano\\Pictures\\Cartelera\\Top Gun.jpg");
+        l1.setBounds(340,50,360,30);
+        l1.setFont(new Font("Arial",Font.BOLD,20));
         
-        //Catalogo Peliculas String
-        pel1 = new String("Dune");
-        pel2 = new String("Thor");
-        pel3 = new String("Joker");
-        pel4 = new String("Top Gun");
-        pel5 = new String("");
-        pel6 = new String("");
-        pel7 = new String("");
-        pel8 = new String("");
-                
-        //Image Scale - Dune
-        peli1 = new JLabel(peliDune);
-        peli1.setBounds(20,120,150,200);
-        Image img = peliDune.getImage();        
-        Image scale = img.getScaledInstance(peli1.getWidth(), peli1.getHeight(), Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon = new ImageIcon(scale);
-        peli1.setIcon(scaledIcon);
+        l2 = new JLabel("Nombre de Pelicula: ");
+        l2.setBounds(270,170,200,20);
+        l2.setFont(new Font("Arial",Font.BOLD,18));
+        tf = new JTextField();
+        tf.setFont(new Font("Arial",Font.PLAIN,14));
+        tf.setBounds(450,165,300,30);
         
-        //image Scale - Thor
-        peli2 = new JLabel(peliThor);
-        peli2.setBounds(180,120,150,200);
-        Image img2 = peliThor.getImage();
-        Image scale2 =img2.getScaledInstance(peli2.getWidth(), peli2.getHeight(), Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon2 = new ImageIcon(scale2);
-        peli2.setIcon(scaledIcon2);
+        l3 = new JLabel("Numero de Sala: ");
+        l3.setBounds(270,200,200,20);
+        l3.setFont(new Font("Arial",Font.BOLD,18));
+        //tf2 = new JTextField();
+        //tf2.setFont(new Font("Arial",Font.PLAIN,14));
+        //tf2.setBounds(450,195,300,30);
         
-        //image Scale Joker
-        peli3 = new JLabel(peliJoker);
-        peli3.setBounds(20,450,150,200);
-        Image img3 = peliJoker.getImage();
-        Image scale3 =img3.getScaledInstance(peli3.getWidth(), peli3.getHeight(), Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon3 = new ImageIcon(scale3);
-        peli3.setIcon(scaledIcon3);
+        l4 = new JLabel("Edad de la Pelicula: ");
+        l4.setBounds(270,230,200,20);
+        l4.setFont(new Font("Arial",Font.BOLD,18));
+        tf3 = new JTextField();
+        tf3.setFont(new Font("Arial",Font.PLAIN,14));
+        tf3.setBounds(450,225,300,30);
         
-        //Image Scale TopGun
-        peli4 = new JLabel(peliTG);
-        peli4.setBounds(180,450,150,200);
-        Image img4 = peliTG.getImage();
-        Image scale4 =img4.getScaledInstance(peli4.getWidth(), peli4.getHeight(), Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon4 = new ImageIcon(scale4);
-        peli4.setIcon(scaledIcon4);
         
-        //Botnes Editar Peliculas
-        
-        //seleccion sala 1
+        //seleccion salas
         sel1 = new JComboBox();
-        sel1.setBounds(55,330,80,30);
+        sel1.setBounds(450,195,300,30);
         sel1.setFont(new Font("Arial",Font.BOLD,16));
-        sel1.addItem(pel1);
-        sel1.addItem(pel2);
-        sel1.addItem(pel3);
-        sel1.addItem(pel4);
+        sel1.addItem(sala1);
+        sel1.addItem(sala2);
+        sel1.addItem(sala3);
+        sel1.addItem(sala4);
         
         sel2 = new JComboBox();
-        sel2.setBounds(210,330,80,30);
+        sel2.setBounds(450,225,300,30);
         sel2.setFont(new Font("Arial",Font.BOLD,16));
-        sel2.addItem(pel2);
-        sel2.addItem(pel1);
-        sel2.addItem(pel3);
-        sel2.addItem(pel4);
+        sel2.addItem(tP);
+        sel2.addItem(aD);
+        sel2.addItem(dM);
+        sel2.addItem(M);
+
+        b1 = new JButton("Agregar");
+        b1.setBounds(270,270,200,20);       
         
-        //Seleccion de Peliculas
+        tbl.addColumn("Nombre De Pelicula");
+        tbl.addColumn("Sala");
+        tbl.addColumn("Edad");
         
+        sc = new JScrollPane();
         
+        mantPeli.setFont(new Font("Arial",Font.PLAIN,16));
+        mantPeli.setBounds(270,300,500,200);
+        sc.setViewportView(mantPeli);
         
-        //Imagenes de peliculas
-        add(peli1);
-        add(peli2);
-        add(peli3);
-        add(peli4);
+        refreshTable();           
+        
+        //Seleccion de Peliculas              
+        add(sc);
+        add(mantPeli);      
         
         //Label Titulo y Seleccion de peliculas
         add(l1); 
+        add(l2);
+        add(l3);
+        add(l4);
+        add(tf);
+        add(b1);
         add(sel1);
-        add(sel2);
+        add(sel2);       
         
         sel1.addActionListener(this);
         sel2.addActionListener(this);
-    }
+        b1.addActionListener(this);
+        
+    }   
     
     
     public void actionPerformed(ActionEvent e){
         
-        //Seleccionador 1
-        if(e.getSource()==sel1){
-            String s = (String)sel1.getSelectedItem();
-            if(s == pel1){
-                peli1.setIcon(peliDune);
-                Image img = peliDune.getImage();        
-                Image scale = img.getScaledInstance(peli1.getWidth(), peli1.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(scale);
-                peli1.setIcon(scaledIcon);
-            }
-            if(s == pel2){
-                peli1.setIcon(peliThor);
-                Image img = peliThor.getImage();        
-                Image scale = img.getScaledInstance(peli1.getWidth(), peli1.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(scale);
-                peli1.setIcon(scaledIcon);
-            }
-            if(s == pel3){
-                peli1.setIcon(peliJoker);
-                Image img = peliJoker.getImage();        
-                Image scale = img.getScaledInstance(peli1.getWidth(), peli1.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(scale);
-                peli1.setIcon(scaledIcon);
-            }
-            if(s == pel4){
-                peli1.setIcon(peliTG);
-                Image img = peliTG.getImage();        
-                Image scale = img.getScaledInstance(peli1.getWidth(), peli1.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(scale);
-                peli1.setIcon(scaledIcon);
-            }
+        if(e.getSource()==b1){
+            try{
+                Peliculas peli = new Peliculas();
+                peli.setPelicula(tf2.getText());
+                peli.setSala(sel1.getSelectedItem().toString());
+                peli.setEdad(sel2.getSelectedItem().toString());
+                listaPeli.add(peli);
+                refreshTable();
+                //tbl.addRow(new Object[]{tf.getText()});
             
-        }  
-        
-        //seleccionador 2
-        if(e.getSource()==sel2){
-            String s = (String)sel2.getSelectedItem();
-            if(s == pel1){
-                peli2.setIcon(peliDune);
-                Image img = peliDune.getImage();        
-                Image scale = img.getScaledInstance(peli2.getWidth(), peli2.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(scale);
-                peli2.setIcon(scaledIcon);
+            } catch (Exception g){
+                JOptionPane.showMessageDialog(this,"Hay un dato sin llenar!");
             }
-            if(s == pel2){
-                peli2.setIcon(peliThor);
-                Image img = peliThor.getImage();        
-                Image scale = img.getScaledInstance(peli2.getWidth(), peli2.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(scale);
-                peli2.setIcon(scaledIcon);
-            }
-            if(s == pel3){
-                peli2.setIcon(peliJoker);
-                Image img = peliJoker.getImage();        
-                Image scale = img.getScaledInstance(peli2.getWidth(), peli2.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(scale);
-                peli2.setIcon(scaledIcon);
-            }
-            if(s == pel4){
-                peli2.setIcon(peliTG);
-                Image img = peliTG.getImage();        
-                Image scale = img.getScaledInstance(peli2.getWidth(), peli2.getHeight(), Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(scale);
-                peli2.setIcon(scaledIcon);
-            }
-            
-            
-            
+        }       
+    }
+    
+    public void refreshTable(){
+        //Borrar todos los elementos del modelo, se hace para que no se dupliquen
+        while(tbl.getRowCount()>0){
+            tbl.removeRow(0);
         }
+        
+        for(Peliculas pel : listaPeli){
+            Object o[] = new Object[3];
+            o[0] = pel.getPelicula();
+            o[1] = pel.getSala();
+            o[2] = pel.getEdad();
+            tbl.addRow(o);
+        }
+        
+        mantPeli.setModel(tbl);
     }
         
     public void stateChanged(ChangeEvent f){
